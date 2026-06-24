6d1aad5bf32fcd885b2e26c6ae5621cfd8abf377
Ekt <ekaterina0196@yandex.ru>
2026-06-24 22:42:34 +0300

    fix: remove unnecessary files

--- Extensions/SplashViewController+AuthViewControllerDelegate.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-extension SplashViewController: AuthViewControllerDelegate {
-    func didAuthenticate(_ vc: AuthViewController) {
-        vc.dismiss(animated: true)
-        switchToTabBarController()
-    }
-}
