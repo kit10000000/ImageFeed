184a778acdd48e696f013714a4ed1751191b32e5
Ekt <ekaterina0196@yandex.ru>
2026-06-27 22:07:01 +0300

    fix: correctly pop WebView before starting token fetch

--- Auth/AuthViewController.swift
+++ Auth/AuthViewController.swift
@@ -60,6 +60,7 @@ final class AuthViewController: UIViewController {
 
 extension AuthViewController: WebViewViewControllerDelegate {
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
+        vc.navigationController?.popViewController(animated: true)
         UIBlockingProgressHUD.show()
         OAuth2Service.shared.fetchOAuthToken(code: code) { [weak self] result in
             UIBlockingProgressHUD.dismiss()
